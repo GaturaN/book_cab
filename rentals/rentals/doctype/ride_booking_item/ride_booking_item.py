722b4decc03219cd3709c2b802f07210f7e4fe35
Francis Njenga <gaturanjenga@gmail.com>
2024-07-17 06:17:30 +0300

    Added ride booking childtable

---
+++ rentals/rentals/doctype/ride_booking_item/ride_booking_item.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, FGN and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class RideBookingItem(Document):
+	pass
