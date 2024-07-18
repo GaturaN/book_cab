357096e281dfdd7444bb87aa3e189141baf5ce2f
Francis Njenga <gaturanjenga@gmail.com>
2024-07-18 15:46:45 +0300

    Updated bookings

--- rentals/rentals/doctype/ride_booking/ride_booking.js
+++ rentals/rentals/doctype/ride_booking/ride_booking.js
@@ -2,19 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Ride Booking", {
-  // refresh: function(frm) {
-  // }
+  refresh: function (frm) {},
+  rate(frm) {
+    frm.trigger("update_total_amount");
+  },
+
+  update_total_amount(frm) {
+    let total_d = 0;
+    for (let item of frm.doc.items) {
+      // console.log(item.distance_covered);
+      total_d += item.distance_covered;
+    //   console.log(total_d);
+    }
+    const amount = frm.doc.rate * total_d;
+    frm.set_value("total_amount", "amount");
+    frm.save();
+  },
 });
 
 frappe.ui.form.on("Ride Booking Item", {
-  refresh: function (frm) {
-
-  },
+  refresh: function (frm) {},
 
   distance_covered(frm, cdt, cdn) {
     // console.log(cdt, cdn);
     // console.log("Child Table Distance Changed!");
-	// console.log(frappe.get_doc(cdt, cdn))
-	// frappe.model.set_value(cdt, cdn, "source", "Updated Source")
+    // console.log(frappe.get_doc(cdt, cdn))
+    // frappe.model.set_value(cdt, cdn, "source", "Updated Source")
+    frm.trigger("update_total_amount");
   },
 });
