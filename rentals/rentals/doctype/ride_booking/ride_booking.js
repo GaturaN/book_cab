20c379324f8e6b57b633081a93b6aa3275c0bb73
Francis Njenga <gaturanjenga@gmail.com>
2024-07-18 13:01:02 +0300

    Updated Ride Booking Childtable .js

--- rentals/rentals/doctype/ride_booking/ride_booking.js
+++ rentals/rentals/doctype/ride_booking/ride_booking.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, FGN and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Ride Booking', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Ride Booking", {
+  // refresh: function(frm) {
+  // }
+});
 
-	// }
+frappe.ui.form.on("Ride Booking Item", {
+  refresh: function (frm) {
+
+  },
+
+  distance_covered(frm, cdt, cdn) {
+    // console.log(cdt, cdn);
+    // console.log("Child Table Distance Changed!");
+	// console.log(frappe.get_doc(cdt, cdn))
+	// frappe.model.set_value(cdt, cdn, "source", "Updated Source")
+  },
 });
--- rentals/rentals/doctype/ride_order/ride_order.js
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2024, FGN and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Ride Order', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Ride Order", {
+  refresh: function (frm) {
+    // if status !== Accepted
+    if (frm.doc.status === "New") {
+      // add custom button
+      frm.add_custom_button(
+        "Accept",
+        () => {
+          // set status to accepted
+          frm.set_value("status", "Accepted");
+          //  save form
+          frm.save();
+        },
+        "Actions"
+      );
 
-	// }
+      frm.add_custom_button(
+        "Reject",
+        () => {
+          // set status to rejected
+          frm.set_value("status", "Rejected");
+          //  save form
+          frm.save();
+        },
+        "Actions "
+      );
+    }
+  },
 });
