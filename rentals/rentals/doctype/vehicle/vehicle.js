9c04342a1f6e48a1194cc339a42bb933fdd86932
GaturaN <gaturanjenga@gmail.com>
2024-07-22 21:02:49 +0300

    Update

--- rentals/rentals/doctype/vehicle/vehicle.js
+++ rentals/rentals/doctype/vehicle/vehicle.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Vehicle', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+
+	get_summary(frm){
+
+		if (!frm.get_field("summary").$wrapper.find("h1").length) {
+			frm.get_field("summary").$wrapper.append("<h1>Here is your summary</h1>");
+		}
+	}
 });
